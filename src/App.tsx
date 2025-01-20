f759cb371552180ce9bffb1ad5266a8d0e01c798
kieldoescode30 <kielycarreon123@gmail.com>
2025-01-20 08:27:03 +0800

    feat: added pokemon regions navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import "./styles/main.scss";
+import { Carousel } from "./components/carousel";
+import { slideData } from "./constants/data";
 
 const App = () => {
   return (
     <main className="main-container">
-      <div className="left">
-        <div className="logo-container">
-          <img src="/pokeball-icon.png" alt="pokeball" />
-          <span>Wébdex</span>
-        </div>
-
-        <div className="pokemon-display">
-          <img src="/132.png" alt="bulbasaur" />
-        </div>
+      <div className="logo-container">
+        <img src="/pokeball-icon.png" alt="pokeball" />
+        <span>Wébdex</span>
       </div>
+
+      <div className="absolute w-full top-[50%] transform translate-y-[-50%] z-10">
+        <Carousel slides={slideData} />
+      </div>
+
       <div className="circle__one" />
-      <div className="circle__two" />
+      <div className="circle__red" />
       <div className="circle__three" />
+
+      <div className="circle__orange" />
     </main>
   );
 };
---
+++ src/constants/data.ts
@@ -0,0 +1,52 @@
+export const slideData = [
+  {
+    title: "Kanto",
+    button: "View",
+    src: "/kanto-region.png",
+  },
+  {
+    title: "Johto",
+    button: "View",
+    src: "/johto-region.png",
+  },
+  {
+    title: "Hoenn",
+    button: "View",
+    src: "/hoenn-region.png",
+  },
+  {
+    title: "Sinnoh",
+    button: "View",
+    src: "/sinnoh-region.png",
+  },
+  {
+    title: "Unova",
+    button: "View",
+    src: "/unova-region.png",
+  },
+  {
+    title: "Kalos",
+    button: "View",
+    src: "/kalos-region.png",
+  },
+  {
+    title: "Alola",
+    button: "View",
+    src: "/alola-region.webp",
+  },
+  {
+    title: "Galar",
+    button: "View",
+    src: "/galar-region.png",
+  },
+  {
+    title: "Hisui",
+    button: "View",
+    src: "/hisui-region.png",
+  },
+  {
+    title: "Paldea",
+    button: "View",
+    src: "/paldea-region.png",
+  },
+];

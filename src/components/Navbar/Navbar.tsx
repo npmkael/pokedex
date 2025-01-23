c75613153b5a7e29da99f3b2f7ae99ba962b12c5
kieldoescode30 <kielycarreon123@gmail.com>
2025-01-23 17:06:02 +0800

    feat: left side of navbar done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,66 @@
+import { Moon } from "lucide-react";
 import "./Navbar.scss";
+import { useState } from "react";
+
+import { motion } from "motion/react";
+
+const transition = {
+  type: "spring",
+  mass: 0.5,
+  damping: 11.5,
+  stiffness: 100,
+  restDelta: 0.001,
+  restSpeed: 0.001,
+};
 
 const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
-    <header className="bg-white/40 sticky top-0 z-40 backdrop-blur-lg w-full border border-b-gray-200/10">
-      <div className="container flex h-24 items-center justify-center">
-        {/* left */}
-        <div className="left">
-          <div className="pokemon-nav"></div>
+    <header className="bg-white/40 sticky top-0 z-40 backdrop-blur-lg left-0 right-0 border border-gray-100">
+      <div className="container flex h-24 justify-between items-center w-full">
+        <div
+          className="left h-full flex items-center"
+          onMouseEnter={() => setIsActive(true)}
+          onMouseLeave={() => setIsActive(false)}
+        >
+          <div className="pokemon-nav">
+            <img src="/pokeball-nav.svg" alt="" width="30" height="30" />
+            <span>Pokémon Data</span>
+          </div>
+
+          {isActive && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.85, y: 10 }}
+              animate={{ opacity: 1, scale: 1, y: 0 }}
+              transition={transition}
+              className="menu left"
+            >
+              <ul>
+                <li>
+                  <a href="#">Pokédex</a>
+                </li>
+                <li>Coming Soon!</li>
+                <li>Coming Soon!</li>
+                <li>Coming Soon!</li>
+              </ul>
+            </motion.div>
+          )}
         </div>
 
         <div className="logo-container">
-          <img src="/pokeball-icon.png" alt="pokeball" />
-          <span>Wébdex</span>
+          <div className="flex justify-center items-center gap-[6px]">
+            <img src="/pokeball-icon.png" alt="pokeball" />
+            <span>Wébdex</span>
+          </div>
 
-          <div className="beta">BETA</div>
+          {/* <div className="beta">BETA</div> */}
+        </div>
+
+        <div className="right">
+          <div className="nav-item">
+            <Moon />
+          </div>
         </div>
       </div>
     </header>

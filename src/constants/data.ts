4f664586b65c69dad4f81489e95dd941c93b1034
kieldoescode30 <kielycarreon123@gmail.com>
2025-01-26 17:55:45 +0800

    refactor: version links

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -2,12 +2,12 @@ export const pokemonGenerations = [
   {
     imgUrl: "/all-pokemon.jpg",
     versionName: "All Pokemon",
-    versionLink: "/all",
+    versionLink: "all",
   },
   {
     imgUrl: "/pokemon-red-blue-yellow.jpg",
     versionName: "Pokémon Red, Blue & Yellow",
-    versionLink: "/red-blue-yellow",
+    versionLink: "red-blue-yellow",
     regionName: "Kanto",
   },
   {
@@ -19,37 +19,37 @@ export const pokemonGenerations = [
   {
     imgUrl: "/pokemon-ruby-sapphire-emerald.jpg",
     versionName: "Pokémon Ruby, Sapphire & Emerald",
-    versionLink: "/ruby-sapphire-emerald",
+    versionLink: "ruby-sapphire-emerald",
     regionName: "Hoenn",
   },
   {
     imgUrl: "/firered-leafgreen.jpg",
     versionName: "Pokémon FireRed & LeafGreen",
-    versionLink: "/firered-leafgreen",
+    versionLink: "firered-leafgreen",
     regionName: "Kanto",
   },
   {
     imgUrl: "/diamond-pearl.jpg",
     versionName: "Pokémon Diamond & Pearl",
-    versionLink: "/diamond-pearl",
+    versionLink: "diamond-pearl",
     regionName: "Sinnoh",
   },
   {
     imgUrl: "/platinum.jpg",
     versionName: "Pokémon Platinum",
-    versionLink: "/platinum",
+    versionLink: "platinum",
     regionName: "Sinnoh",
   },
   {
     imgUrl: "/heartgold-soulsilver.jpg",
     versionName: "Pokémon HeartGold & SoulSilver",
-    versionLink: "/heartgold-soulsilver",
+    versionLink: "heartgold-soulsilver",
     regionName: "Johto",
   },
   {
     imgUrl: "/black-white.jpg",
     versionName: "Pokémon Black & White",
-    versionLink: "/black-white",
+    versionLink: "black-white",
     regionName: "Unova",
   },
 ];

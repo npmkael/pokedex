b43ba27682981d25d64b1dc65c1b4f45eb1700aa
kieldoescode30 <kielycarreon123@gmail.com>
2025-02-03 20:03:46 +0800

    feat: function for checking pokemon's type (initial)

--- src/components/pokemon-type.tsx
+++ src/components/pokemon-type.tsx
@@ -1,4 +1,5 @@
 import { Leaf } from "lucide-react";
+import { color } from "motion/react";
 import React from "react";
 
 type PokemonTypeProps = {
@@ -6,7 +7,14 @@ type PokemonTypeProps = {
 };
 
 const PokemonType = () => {
-  const checkType = (type: string) => {};
+  const checkType = (type: string) => {
+    switch (type) {
+      case "grass":
+        return <div></div>;
+    }
+  };
+
+  const type = checkType("grass");
 
   return (
     <div className="w-[110px] bg-[#7C5] rounded-md flex gap-2 items-center justify-center">
